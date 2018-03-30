5ff68600ea0e433ffff80c187f5bd79e03d7f58f
codeb1ooded <meghaaggarwal493@gmail.com>
2018-03-31 00:33:20 +0530

    Safest route

--- app/src/main/java/com/aarushi/crime_mappingapp/safest_route/SafestRouteActivity.java
+++ app/src/main/java/com/aarushi/crime_mappingapp/safest_route/SafestRouteActivity.java
@@ -1,10 +1,14 @@
 package com.aarushi.crime_mappingapp.safest_route;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.aarushi.crime_mappingapp.API.ApiClient;
@@ -15,14 +19,19 @@ import com.aarushi.crime_mappingapp.Models.path.Path;
 import com.aarushi.crime_mappingapp.Models.path.Route;
 import com.aarushi.crime_mappingapp.Models.path.Step;
 import com.aarushi.crime_mappingapp.R;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.common.api.Status;
 
 import java.util.ArrayList;
 
@@ -36,15 +45,44 @@ public class SafestRouteActivity extends FragmentActivity implements OnMapReadyC
     private GPSTracker mGPSTracker;
 
     private double mLatitude, mLongitude;
+    private ProgressDialog progressDialog;
+    private Marker marker;
+    private ArrayList<Polyline> polylines;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_safest_route);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                // TODO: Get info about the selected place.
+                Log.i("SafestRouteActivity", "Place: " + place.getName());
+                if(marker != null)  marker.remove();
+                MarkerOptions options = new MarkerOptions()
+                        .title("My Position")
+                        .position(place.getLatLng());
+                marker = mMap.addMarker(options);
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(place.getLatLng(),8.0f));
+                getDirection(mLatitude + "," + mLongitude,  place.getLatLng().latitude + "," + place.getLatLng().longitude);
+
+            }
+
+            @Override
+            public void onError(Status status) {
+                Toast.makeText(SafestRouteActivity.this, "Unable to read. Try Again!", Toast.LENGTH_SHORT).show();
+                Log.i("SafestRouteActivity", "An error occurred: " + status);
+            }
+        });
     }
 
 
@@ -72,18 +110,22 @@ public class SafestRouteActivity extends FragmentActivity implements OnMapReadyC
 
             LatLng latLng = new LatLng(mLatitude, mLongitude);
             mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,8.0f));
-            getDirection(mLatitude + "," + mLongitude, "faridabad");
+
         }else{
             mGPSTracker.showSettingsAlert();
         }
     }
 
     public void getDirection(String origin, String destination){
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Finding the safest route");
+        progressDialog.show();
         Call<Path> getDirections = ApiClient.getInterface().getShortestPath(origin, destination);
         getDirections.enqueue(new Callback<Path>() {
             @Override
             public void onResponse(Call<Path> call, Response<Path> response) {
                 if (response.isSuccessful()) {
+                    progressDialog.dismiss();
                     ArrayList<Route> routes = response.body().getRoutes();
                     if (routes.size() == 0){
                         Toast.makeText(SafestRouteActivity.this, "No route found!", Toast.LENGTH_SHORT).show();
@@ -101,6 +143,7 @@ public class SafestRouteActivity extends FragmentActivity implements OnMapReadyC
                     }
                 }
                 else{
+                    progressDialog.dismiss();
                     Toast.makeText(SafestRouteActivity.this, "Try Again!", Toast.LENGTH_SHORT).show();
                 }
             }
@@ -113,6 +156,12 @@ public class SafestRouteActivity extends FragmentActivity implements OnMapReadyC
     }
 
     public void createRoute(Route route){
+        polylines = new ArrayList<>();
+        if(polylines != null){
+            for(Polyline line: polylines){
+                line.remove();
+            }
+        }
         ArrayList<Step> steps = route.getLegs().get(0).getSteps();
         for(Step step: steps) {
             Polyline line = mMap.addPolyline(new PolylineOptions()
@@ -120,6 +169,7 @@ public class SafestRouteActivity extends FragmentActivity implements OnMapReadyC
                             new LatLng(step.getEndLocation().getLat(), step.getEndLocation().getLng()))
                     .width(5)
                     .color(Color.RED));
+            polylines.add(line);
         }
     }
 
